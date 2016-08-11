5c37beb75c47304bd018510d900fb0ba299c46a9
Zhongyu Li <zli@localhost.localdomain>
2016-08-11 17:36:50 -0500

    deploy tools

--- deploy-test.py
+++ deploy-test.py
@@ -1,11 +1,17 @@
 import os
 def main():
-	
-	try:
-		os.system('git checkout gh-pages')
-	except:
-		break
-
+	commit_msg=input('Please enter the deploy commit msg: ')
+	command_list=['git checkout master',
+			'git status',
+			'git add .',
+			'git status',
+			'git commit -m "%s"'%commit_msg,
+			'git push origin master',
+			'git push origin master:gh-pages -f',
+			'git push coding master:coding-pages -f',
+			'git status']
+	for each in command_list:
+		os.system(each)
 
 
 if __name__ == '__main__':
