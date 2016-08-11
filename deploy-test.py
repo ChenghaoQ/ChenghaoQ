fd96e9de1084a3f2fccb5a52e4830526571f27af
Zhongyu Li <zli@localhost.localdomain>
2016-08-11 17:50:09 -0500

    test

--- deploy-test.py
+++ deploy-test.py
@@ -1,14 +1,16 @@
 import os
 def main():
+	username=input('Your Username: ')
+	passwd = input('Your passwd: ')
 	commit_msg=input('Please enter the deploy commit msg: ')
 	command_list=['git checkout master',
 			'git status',
 			'git add .',
 			'git status',
 			'git commit -m "%s"'%commit_msg,
-			'git push origin master',
-			'git push origin master:gh-pages -f',
-			'git push coding master:coding-pages -f',
+			'git push origin master','%s'%username,
+			'git push origin master:gh-pages -f','%s'%username,
+			'git push coding master:coding-pages -f','%s'%username,
 			'git status']
 	for each in command_list:
 		os.system(each)
